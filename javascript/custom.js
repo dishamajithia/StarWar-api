fca3d85fe1be1f4b4ecbbb18eb696ae5c584bbcc
dishamajithia <majithiadisha93@gmail.com>
2019-12-26 22:47:37 +0530

    updated search using api

--- javascript/custom.js
+++ javascript/custom.js
@@ -1,40 +1,45 @@
 //store fetched data in array
 var loadedData = [];
 var page = 1;
+
 //dynamic list li generating for search result
 function generateListItem(item, idx) {
     function createAttribute(label, value) {
-        return $(`<span>`).addClass('P-10 M-5').text(`${label}: ${value}`);
-    };
+        return $(`<span>`)
+            .addClass("P-10 M-5")
+            .text(`${label}: ${value}`);
+    }
     const labelMap = {
-        height: 'Height',
-        mass: 'Mass',
-        hair_color: 'Hair Color',
-        skin_color: 'Skin Color',
-        eye_color: 'Eye Color',
-        birth_year: 'Birth Year',
-        gender: 'Gender'
+        height: "Height",
+        mass: "Mass",
+        hair_color: "Hair Color",
+        skin_color: "Skin Color",
+        eye_color: "Eye Color",
+        birth_year: "Birth Year",
+        gender: "Gender"
     };
-    const listItem = $('<li>').addClass('list-item people w-50 m-auto');
-    const container = $('<div>').addClass('P-10 M-5');
-    const title = $('<div>').addClass('title');
-    const collapseLink = $('<a>', {
-        'data-target': `#item${idx}`,
-        'data-toggle': 'collapse'
+    const listItem = $("<li>").addClass("list-item people w-50 m-auto");
+    const container = $("<div>").addClass("P-10 M-5");
+    const title = $("<div>").addClass("title");
+    const collapseLink = $("<a>", {
+        "data-target": `#item${idx}`,
+        "data-toggle": "collapse"
     });
-    const titleHeader = $('<h2>').addClass('name').text(`${item.name}`)
+    const titleHeader = $("<h2>")
+        .addClass("name")
+        .text(`${item.name}`);
     titleHeader.text(item.name);
-    collapseLink.append(titleHeader)
+    collapseLink.append(titleHeader);
     title.append(collapseLink);
-    container.append(title)
+    container.append(title);
     listItem.append(container);
 
-    const collapsibleContent = $('<div>', {
-        class: 'collapse-item content-wrap collapse',
+    const collapsibleContent = $("<div>", {
+        class: "collapse-item content-wrap collapse",
         id: `item${idx}`
     });
-    const content = $('<div>', {
-        class: 'content'
+    const content = $("<div>", {
+        class: "content"
     });
     for (const prop in labelMap) {
         const label = labelMap[prop];
@@ -47,21 +52,22 @@ function generateListItem(item, idx) {
 }
 //populate data from api
 function loadData(data) {
-    const list = $('#list');
+    const list = $("#list");
     data.forEach((item, idx) => {
         const listItem = generateListItem(item, idx);
         list.append(listItem);
     });
 }
-//fetch data from api 
+//fetch data from api
 function getdata(pagenumber) {
-    console.log(pagenumber)
+    console.log(pagenumber);
     //loadedData = [];
     //Create the XHR Object
-    let xhr = new XMLHttpRequest;
+    let xhr = new XMLHttpRequest();
 
     //Call the open function, GET-type of request, url, true-asynchronous
-    xhr.open('GET', 'https://swapi.co/api/people/?page=' + pagenumber, true)
+
+    xhr.open("GET", "https://swapi.co/api/people/?page=" + pagenumber, true);
     //call the onload
     xhr.onload = function () {
         //check if the status is 200(means everything is okay)
@@ -69,39 +75,54 @@ function getdata(pagenumber) {
             $(".loadMore").css("display", "block");
             const data = JSON.parse(this.responseText);
             loadedData.push(...data.results);
-            loadData(data.results)
+            loadData(data.results);
             if (data.next) {
                 page += 1;
             } else {
-                $(".loadMore").hide()
+                $(".loadMore").hide();
             }
         }
-    }
+    };
     //call send
     xhr.send();
-
 }
+
+
 
 $(document).ready(function () {
     $(".loadMore").on("click", function () {
         getdata(page);
-    })
-
+    });
     getdata(page);
-    //search data
-    $("#searchid").keyup(function () {
-        var filter = $(this).val();
-        $(".search-view-wrapper .search-list ul li h2").each(function () {
 
-            if ($(this).text().search(new RegExp(filter, "i")) < 0) {
-                $(this).closest(".people").addClass("d-none");
+    $("#searchid").on("keyup", function () {
+        $("#list").html('<li style="color: white; text-align: center;">Searching...</li>');
+        if ($(this).val() === "") {
+            $("#list").html("");
+            console.log(loadedData)
+            loadData(loadedData);
+            return;
+        }
 
-            } else {
-                $(this).closest(".people").removeClass("d-none");
-                $(this).show();
-
+        var searchTerm = $(this).val();
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://swapi.co/api/people/?search=" + searchTerm, true);
+        xhr.onreadystatechange = function () {
+            if (this.status === 200) {
+                console.log(this.responseText)
+                if (this.responseText) {
+                    const data = JSON.parse(this.responseText);
+                    if (data.results.length == 0) {
+                        $("#list").html("");
+                        $("#list").html('<li style="color: white; text-align: center;">No results found</li>');
+                    } else {
+                        $("#list").html("");
+                    }
+                    loadData(data.results);
+                }
             }
-            $("#peopletemplate").addClass("d-none");
-        });
+        };
+        xhr.send();
+        $("#peopletemplate").addClass("d-none");
     });
 });
